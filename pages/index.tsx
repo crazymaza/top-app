5d32d3faee5ff04eb065df250acc9ae1d08b39c8
Igor Valnev <iavalnev@sberbank.ru>
2022-12-15 17:10:52 +0300

    added Htag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image';
+import { Htag } from "../components";
 
 export default function Home(): JSX.Element {
-
-  return (<></>);
+  return (<Htag tag="h1">Text</Htag>);
 }
