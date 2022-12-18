cc676295ee77a6c9993cf65fe0982d6f9692d7e5
Igor Valnev <iavalnev@sberbank.ru>
2022-12-18 16:51:58 +0300

    added css grid from layout and footer

--- layout/footer/Footer.tsx
+++ layout/footer/Footer.tsx
@@ -1,8 +1,13 @@
 import { FooterProps } from './FooterProps';
+import styles from './Footer.module.css';
+import cn from 'classnames';
+import {format} from 'date-fns';
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>);
+        <footer className={cn(styles.footer, className)} {...props}>
+            <span className={styles.allRight}>OwlTop 2022-{format(new Date(), 'yyyy')} Все права защищены</span>
+            <a href='#' className={styles.users}>Пользовательское соглашение</a>
+            <a href='#' className={styles.confidencial}>Политика конфиденциальности</a>
+        </footer>);
 };
