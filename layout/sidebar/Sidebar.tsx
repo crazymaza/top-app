6899d922d30c483e51f80c4ed5602874b78b9ccf
Igor Valnev <iavalnev@sberbank.ru>
2022-12-24 19:24:33 +0300

    added styles to sidebar

--- layout/sidebar/Sidebar.tsx
+++ layout/sidebar/Sidebar.tsx
@@ -1,9 +1,17 @@
 import { Menu } from '../menu/Menu';
 import { SidebarProps } from './SidebarProps';
+import Logo from '../logo.svg';
+import cn from 'classnames';
+import styles from './Sidebar.module.css';
 
-export const Sidebar = ({...props}: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
     return (
-        <div {...props}>
+        <div {...props} className={cn(className, styles.sidebar)}>
+            <div className={styles.logoWrapper}>
+            <Logo/>
+            <span>OWLtop</span>
+            </div>
+            <div>Поиск</div>
             <Menu />
         </div>);
 };
